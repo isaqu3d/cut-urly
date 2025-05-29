62d0a12865e2fc09a4c7e4ebc1906a02bea25455
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 11:51:42 -0300

    refactor: refactor UrlController to include a new endpoint for shortening URLs

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UrlService } from './url.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUrlDto } from './dto/create-url.dto';
-import { UpdateUrlDto } from './dto/update-url.dto';
+import { UrlService } from './url.service';
 
-@Controller('url')
+@Controller()
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
-  @Post()
-  create(@Body() createUrlDto: CreateUrlDto) {
+  @Post('shorten-url')
+  create(@Body('url') createUrlDto: CreateUrlDto) {
     return this.urlService.create(createUrlDto);
   }
 
-  @Get()
-  findAll() {
-    return this.urlService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(':shortCode')
+  findOne(@Param('shortCode') id: string) {
     return this.urlService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUrlDto: UpdateUrlDto) {
-    return this.urlService.update(+id, updateUrlDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.urlService.remove(+id);
   }
 }
