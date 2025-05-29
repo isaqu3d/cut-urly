1d0b4d275cd0d75654e75d2db264b1338dd4d8ca
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 11:52:46 -0300

    chore: add TypeOrmModule for Url entity in UrlModule

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UrlService } from './url.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Url } from './entities/url.entity';
 import { UrlController } from './url.controller';
+import { UrlService } from './url.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Url])],
   controllers: [UrlController],
   providers: [UrlService],
 })
