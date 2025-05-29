698405c16e7e4d5b2b451b62b33a16530abb0533
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 11:54:40 -0300

    chore: remove unused methods from UrlService

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUrlDto } from './dto/create-url.dto';
-import { UpdateUrlDto } from './dto/update-url.dto';
 
 @Injectable()
 export class UrlService {
@@ -8,19 +7,7 @@ export class UrlService {
     return 'This action adds a new url';
   }
 
-  findAll() {
-    return `This action returns all url`;
-  }
-
   findOne(id: number) {
     return `This action returns a #${id} url`;
-  }
-
-  update(id: number, updateUrlDto: UpdateUrlDto) {
-    return `This action updates a #${id} url`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} url`;
   }
 }
