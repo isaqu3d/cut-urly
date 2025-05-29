1d07226163ae022acf303607846ec9dddb4c15c7
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 12:03:45 -0300

    feat: add Url entity repository injection to UrlService constructor

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUrlDto } from './dto/create-url.dto';
+import { Url } from './entities/url.entity';
 
 @Injectable()
 export class UrlService {
+  constructor(@InjectRepository(Url) private urlRepository: Repository<Url>) {}
+
   create(createUrlDto: CreateUrlDto) {
     return 'This action adds a new url';
   }
