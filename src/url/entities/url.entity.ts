d8d1507b160a121b74569c984f82fd4160d0b20e
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 11:58:01 -0300

    feat: add Url entity with columns for id, shortCode, originalUrl, expiresAt, and createdAt

--- src/url/entities/url.entity.ts
+++ src/url/entities/url.entity.ts
@@ -1,1 +1,24 @@
-export class Url {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Url {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  shortCode: string;
+
+  @Column({ unique: true })
+  originalUrl: string;
+
+  @Column({ type: 'timestamp' })
+  expiresAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
