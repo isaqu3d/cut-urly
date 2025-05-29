c6c12085f870e6820e26a08cab014c42a0115f0d
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 12:11:31 -0300

    feat: add validation for URL field in CreateUrlDto class

--- src/url/dto/create-url.dto.ts
+++ src/url/dto/create-url.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUrlDto {}
+import { IsNotEmpty, IsUrl } from 'class-validator';
+
+export class CreateUrlDto {
+  @IsNotEmpty({ message: 'URL cannot be empty' })
+  @IsUrl({}, { message: 'Invalid URL format' })
+  url: string;
+}
