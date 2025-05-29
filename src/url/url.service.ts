815cf329c6ede7f9028718977d2d9dfee5190fd3
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-29 12:05:04 -0300

    chore: add method to generate short codes for URLs

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -8,6 +8,18 @@ import { Url } from './entities/url.entity';
 export class UrlService {
   constructor(@InjectRepository(Url) private urlRepository: Repository<Url>) {}
 
+  private generateShortCode(length = 6): string {
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let shortCode = '';
+
+    for (let i = 0; i < length; i++) {
+      shortCode += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return shortCode;
+  }
+
   create(createUrlDto: CreateUrlDto) {
     return 'This action adds a new url';
   }
